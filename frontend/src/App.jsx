ee404c20b62fe034f11b6b95d1578f27bd237a5d
PulkitChikara <pulki2010@gmail.com>
2024-01-06 17:42:38 +0530

    backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Slider from './components/SideBar/SideBar.jsx'
-import Dashboard from './pages/Dashboard'
-import ProductsPage from './pages/ProductsPage.jsx'
+import Slider from './components/SideBar/SideBar.jsx';
+import Dashboard from './pages/Dashboard';
+import ProductsPage from './pages/ProductsPage.jsx';
 import SuggestionsPage from './pages/SuggestionsPage.jsx';
+import LoginPage from './pages/login.jsx'; // Corrected filename assuming it's "Login.jsx"
 
 const App = () => {
-    return (
-        <div className='w-full min-h-screen flex bg-slate-200'>
-            <Router>
-
-                <div className='w-fit lg:max-w-[300px] lg:w-full h-full'>
-                    <Slider />
-                </div>
-
-                <Routes>
-                    
-                    <Route exact path="/" element={<ProductsPage />} />
-                    <Route exact path="/dashboard" element={<Dashboard />} />
-                    <Route exact path="/products" element={<ProductsPage />} />
-                    <Route exact path="/suggestions" element={<SuggestionsPage />} />
-
-                </Routes>
-
-            </Router>
+  return (
+    <div className='w-full min-h-screen flex bg-slate-200'>
+      <Router>
+        <div className='w-fit lg:max-w-[300px] lg:w-full h-full'>
+          <Slider />
         </div>
-    )
-}
 
-export default App
+        <Routes>
+            <Route index element={<ProductsPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/suggestions" element={<SuggestionsPage />} />
+            <Route path="/login" element={<LoginPage />} />    
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
